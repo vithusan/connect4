03a73fa6529bcbb5cbadf43115261b9a1ed2a69b
Vithusan Kanageswaran <vk@Vithusans-MacBook-Air.local>
2019-10-03 14:55:25 -0400

    created all the files

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const mainBoard = document.querySelector('.connecterDisplay')
+
+
+
+// for(let i=0; i<42; i++){
+//     const newElement = document.createElement('div')
+//     newElement.setAttribute('class', 'testCir')
+//     mainBoard.appendChild(newElement)
+// }
+
+// for(let i=0; i<4; i++){
+//     const newElement = document.createElement('div')
+//     newElement.setAttribute('class', 'testCir')
+//     newElement.setAttribute('data-pos', i + 1)
+//     mainBoard.appendChild(newElement)
+// }
+
+
--- readme.md
+++ readme.md
@@ -1,2 +1,8 @@
+<h1>Connect Four</h1>
+
+<h2>Overview</h2>
+
+The goal of this project was to be able to use our skills 
+
 ![wireframe](/P1-wireframe.jpg)
 
