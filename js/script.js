be236825168076a6abedbb5c2f61492c14a17c67
Vithusan Kanageswaran <vk@Vithusans-MacBook-Air.local>
2019-10-03 14:56:50 -0400

    created for-loop to make list of circles

--- js/script.js
+++ js/script.js
@@ -15,4 +15,23 @@ const mainBoard = document.querySelector('.connecterDisplay')
 //     mainBoard.appendChild(newElement)
 // }
 
+for(let i=0;i<7;i++){
+    const newElement1 = document.createElement('div')
+    newElement1.setAttribute('class', 'column')
+    newElement1.setAttribute('data-col', i + 1)
+    mainBoard.appendChild(newElement1)
+    for(let k=0;k<6;k++){
+        const newElement2 = document.createElement('div')
+        newElement2.setAttribute('class', 'testCir')
+        newElement2.setAttribute('data-pos', k + 1)
+        newElement1.appendChild(newElement2)
+    }
+}
 
+const circleDiv = document.getElementsByClassName('testCir')
+
+for(let i=0; i<circleDiv.length; i++){
+    circleDiv[i].addEventListener('click', () => {
+        alert(`you've clicked ${i +1}`)
+    })
+}
