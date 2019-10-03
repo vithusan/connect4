2f9dc49cb21e90d0da2e4c6e3224da8e894fded5
Vithusan Kanageswaran <vk@Vithusans-MacBook-Air.local>
2019-10-03 16:30:58 -0400

    revert to smaller board for testing

--- js/script.js
+++ js/script.js
@@ -1,26 +1,40 @@
 const mainBoard = document.querySelector('.connecterDisplay')
+const playersTurn = document.querySelector('.turns')
 
 
-
-// for(let i=0; i<42; i++){
-//     const newElement = document.createElement('div')
-//     newElement.setAttribute('class', 'testCir')
-//     mainBoard.appendChild(newElement)
+// for(let i=0;i<7;i++){
+//     const testRow = document.createElement('div')
+//     testRow.setAttribute('class', 'testRow')
+//     testRow.setAttribute('data-pl', i + 1)
+//     playersTurn.appendChild(testRow)
 // }
 
-// for(let i=0; i<4; i++){
-//     const newElement = document.createElement('div')
-//     newElement.setAttribute('class', 'testCir')
-//     newElement.setAttribute('data-pos', i + 1)
-//     mainBoard.appendChild(newElement)
+for(let i=0;i<2;i++){
+    const testRow = document.createElement('div')
+    testRow.setAttribute('class', 'testRow')
+    testRow.setAttribute('data-pl', i + 1)
+    playersTurn.appendChild(testRow)
+}
+
+// for(let i=0;i<7;i++){
+//     const newElement1 = document.createElement('div')
+//     newElement1.setAttribute('class', 'column')
+//     newElement1.setAttribute('data-col', i + 1)
+//     mainBoard.appendChild(newElement1)
+//     for(let k=0;k<6;k++){
+//         const newElement2 = document.createElement('div')
+//         newElement2.setAttribute('class', 'testCir')
+//         newElement2.setAttribute('data-pos', k + 1)
+//         newElement1.appendChild(newElement2)
+//     }
 // }
 
-for(let i=0;i<7;i++){
+for(let i=0;i<2;i++){
     const newElement1 = document.createElement('div')
     newElement1.setAttribute('class', 'column')
     newElement1.setAttribute('data-col', i + 1)
     mainBoard.appendChild(newElement1)
-    for(let k=0;k<6;k++){
+    for(let k=0;k<2;k++){
         const newElement2 = document.createElement('div')
         newElement2.setAttribute('class', 'testCir')
         newElement2.setAttribute('data-pos', k + 1)
@@ -30,8 +44,18 @@ for(let i=0;i<7;i++){
 
 const circleDiv = document.getElementsByClassName('testCir')
 
-for(let i=0; i<circleDiv.length; i++){
-    circleDiv[i].addEventListener('click', () => {
-        alert(`you've clicked ${i +1}`)
-    })
+// for(let i=0; i<circleDiv.length; i++){
+//     circleDiv[i].addEventListener('click', () => {
+//         alert(`you've clicked ${i +1}`)
+//     })
+// }
+
+const playerClick = document.querySelectorAll('.testRow')
+
+// const testFunction = () => {
+//     if()
+// }
+
+for(let i=0;i<playerClick.length; i++){
+    playerClick[i].addEventListener('click', testFunction)
 }
