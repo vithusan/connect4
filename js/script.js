bfb13b8d16fc494dee8b5b1d0da93542ec19ecbe
Vithusan Kanageswaran <vk@Vithusans-MacBook-Air.local>
2019-10-07 15:29:36 -0400

    diplay the array of values on the page

--- js/script.js
+++ js/script.js
@@ -9,40 +9,13 @@ const playersTurn = document.querySelector('.turns')
 //     playersTurn.appendChild(testRow)
 // }
 
+
 for (let i = 0; i < 4; i++) {
     const testRow = document.createElement('div')
     testRow.setAttribute('class', 'testRow')
     testRow.setAttribute('data-pl', i + 1)
     playersTurn.appendChild(testRow)
 }
-
-// for(let i=0;i<7;i++){
-//     const newElement1 = document.createElement('div')
-//     newElement1.setAttribute('class', 'column')
-//     newElement1.setAttribute('data-col', i + 1)
-//     mainBoard.appendChild(newElement1)
-//     for(let k=0;k<6;k++){
-//         const newElement2 = document.createElement('div')
-//         newElement2.setAttribute('class', 'testCir')
-//         newElement2.setAttribute('data-pos', k + 1)
-//         newElement1.appendChild(newElement2)
-//     }
-// }
-
-
-//make this a function
-// for (let i = 0; i < 4; i++) {
-//     const newElement1 = document.createElement('div')
-//     newElement1.setAttribute('class', 'column')
-
-//     mainBoard.appendChild(newElement1)
-//     for (let k = 0; k < 4; k++) {
-//         const newElement2 = document.createElement('div')
-//         newElement2.setAttribute('class', 'testCir')
-//         newElement2.setAttribute('data-pos', 0)
-//         newElement1.appendChild(newElement2)
-//     }
-// }
 
 let board = [
     [0, 0, 0, 0],
@@ -51,74 +24,67 @@ let board = [
     [0, 0, 0, 0]
 ];
 
-for(let row = 0; row<board.length; row++){
-    for(let pos = 0; pos<board[row].length; pos++){
-        let inidCir = document.createElement('div')
-        inidCir.setAttribute('class', 'eachCir')
-        mainBoard.appendChild(inidCir)
+// let board = [
+//     [0, 0, 0, 0, 0, 0, 0],
+//     [0, 0, 0, 0, 0, 0, 0],
+//     [0, 0, 0, 0, 0, 0, 0],
+//     [0, 0, 0, 0, 0, 0, 0],
+//     [0, 0, 0, 0, 0, 0, 0],
+//     [0, 0, 0, 0, 0, 0, 0]
+// ];
+
+const createBoard = () => {
+    mainBoard.innerHTML = ''
+    for (let row = 0; row < board.length; row++) {
+        let rowElement = document.createElement('div')
+        rowElement.setAttribute('class', 'row')
+        mainBoard.appendChild(rowElement)
+
+        //console.log(row)
+        for (let pos = 0; pos < board[row].length; pos++) {
+            let inidCir = document.createElement('div')
+            inidCir.textContent += board[row][pos]
+            inidCir.setAttribute('class', 'eachCir')
+            rowElement.appendChild(inidCir)
+            // inidCir.addEventListener('click', () => {
+
+            //     board[row][pos] = 1
+            //    changeColorFunction()
+            //     createBoard()
+            //     checkColumn()
+            //     checkRow()
+
+
+            // })
+
+            
+            
+        }
     }
 }
 
-// const circleDiv = document.getElementsByClassName('testCir')
+createBoard()
+runFunction()
+const inidCirBtn = document.getElementsByClassName('eachCir')
 
-// for(let i=0; i<circleDiv.length; i++){
-//     circleDiv[i].addEventListener('click', () => {
-//         alert(`you've clicked ${i +1}`)
-//     })
-// }
+const runFunction=() =>{
+
+    for(let r =0;r<board.length;r++){
+        for(let p=0; p<board[r].length;p++){
+    
+            inidCirBtn.addEventListener('click', () => {
+                console.log(p)
+            })
+            
+        }
+    }
+}
+
+
+const rows = document.getElementsByClassName('row')
 
 
 //MAKE AN ARRAY WITHIN AN ARRAY TO STORE ALL THE CIRCLUES
-
-
-
-
-const columnBtn = document.getElementsByClassName('column')
-const test2 = document.getElementsByClassName('testCir')
-const test11 = document.querySelector('.testCir')
-const topBtn = document.getElementsByClassName('testRow')
-// const columnFunction = () => {
-//     for (let y = 0; y < test1.length; y++) {
-
-//         test1[y].addEventListener('click', ()=>{
-//             //if(test1[y].classList.contains['column']){
-//                 //for(let i=0; i<test2.length; i++){
-//                     if(test11.classList.contains('testCir')){
-//                         test11.classList.add('testCir2')
-//                     }
-//                     console.log(test11)
-//                 //}
-//             //}
-
-//         })
-
-
-
-//         // for (let i = 0; i < test2.length; i++) {
-//         //     //console.log(test2[i])
-//         //     //test2[i].classList.add('testCir2')
-//         //     // if(test2[i].classList.contains('testCir2')){
-//         //     //     console.log('red')
-//         //     // }
-//         //     //console.log(test1[y])
-//         //     test1[y].addEventListener('click', () => {
-//         //         if (test1[y][i].classList.contains('testCir')) {
-//         //             test1[y][i].classList.add('testCir2')
-//         //             player1Array.push(test2[i])
-//         //         }
-//         //     })
-
-//         //     //    if(player1Array.length > 0  && player1Array[i].classList.contains('testCir2')){
-//         //     //        alert('done')
-//         //     //    }
-//         // }
-//     }
-// }
-
-//const connect4 = [test1]
-
-
-
 
 // let board = [
 //     [1, 5, 9, 13],
@@ -183,16 +149,16 @@ const topBtn = document.getElementsByClassName('testRow')
 function checkColumn() {
 
     //row
-    for (let i = 0; i < columnBtn.length; i++) {
-        for (let j = 0; j < test2[i].length; j++) {
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board[i].length; j++) {
 
-            if (test2[j].classList.contains('testCir') 
-                // test2[j] == test2[j + 1] &&
-                // test2[j] == test2[j + 2] &&
-                // test2[j] == test2[j + 3]
+            if (board[i][j] != 0 &&
+                board[i][j] == board[i][j + 1] &&
+                board[i][j] == board[i][j + 2] &&
+                board[i][j] == board[i][j + 3]
             ) {
                 console.log('column')
-                console.log(test2[j])
+                console.log(board)
             }
         }
     }
@@ -212,36 +178,11 @@ function checkRow() {
                 board[j][i] == board[j + 3][i]
             ) {
                 console.log('row')
-                console.log(board[j][i])
+                console.log(board)
             }
 
         }
 
     }
 }
-function clickCir() {
-
-    for (let i = 0; i < columnBtn.length; i++) {
-        for (let j = 0; j < test2.length; j++) {
-            test2[j].addEventListener('click', (e) => {
-    
-           
-                e.target.classList.add('testCir2')
-
-                checkRow()
-                checkColumn()
-               
-            })
-        }
-    }
-
-}
-
-
-
-//checkRow()
-//checkColumn()
-clickCir()
-
-//columnFunction()
 
