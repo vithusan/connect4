4d6fb848399aee59a0a3b4c92154b7596eb450c0
Vithusan Kanageswaran <vk@Vithusans-MacBook-Air.local>
2019-10-07 12:43:57 -0400

    creating a nested loop with rows instead of columns

--- js/script.js
+++ js/script.js
@@ -31,16 +31,31 @@ for (let i = 0; i < 4; i++) {
 
 
 //make this a function
-for (let i = 0; i < 4; i++) {
-    const newElement1 = document.createElement('div')
-    newElement1.setAttribute('class', 'column')
-    newElement1.setAttribute('data-col', i + 1)
+// for (let i = 0; i < 4; i++) {
+//     const newElement1 = document.createElement('div')
+//     newElement1.setAttribute('class', 'column')
 
-    mainBoard.appendChild(newElement1)
-    for (let k = 0; k < 4; k++) {
-        const newElement2 = document.createElement('div')
-        newElement2.setAttribute('class', 'testCir')
-        newElement1.appendChild(newElement2)
+//     mainBoard.appendChild(newElement1)
+//     for (let k = 0; k < 4; k++) {
+//         const newElement2 = document.createElement('div')
+//         newElement2.setAttribute('class', 'testCir')
+//         newElement2.setAttribute('data-pos', 0)
+//         newElement1.appendChild(newElement2)
+//     }
+// }
+
+let board = [
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0],
+    [0, 0, 0, 0]
+];
+
+for(let row = 0; row<board.length; row++){
+    for(let pos = 0; pos<board[row].length; pos++){
+        let inidCir = document.createElement('div')
+        inidCir.setAttribute('class', 'eachCir')
+        mainBoard.appendChild(inidCir)
     }
 }
 
@@ -55,13 +70,12 @@ for (let i = 0; i < 4; i++) {
 
 //MAKE AN ARRAY WITHIN AN ARRAY TO STORE ALL THE CIRCLUES
 
-const player1Array = []
+
 
 
 const columnBtn = document.getElementsByClassName('column')
 const test2 = document.getElementsByClassName('testCir')
 const test11 = document.querySelector('.testCir')
-
 const topBtn = document.getElementsByClassName('testRow')
 // const columnFunction = () => {
 //     for (let y = 0; y < test1.length; y++) {
@@ -104,12 +118,7 @@ const topBtn = document.getElementsByClassName('testRow')
 //const connect4 = [test1]
 
 
-let board = [
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0],
-    [0, 0, 0, 0]
-];
+
 
 // let board = [
 //     [1, 5, 9, 13],
@@ -129,76 +138,110 @@ let board = [
 
 
 
-function checkRow() {
+// function checkColumn() {
+
+//     //row
+//     for (let i = 0; i < board.length; i++) {
+//         for (let j = 0; j < board[i].length; j++) {
+
+//             if (board[i][j] != 0 &&
+//                 board[i][j] == board[i][j + 1] &&
+//                 board[i][j] == board[i][j + 2] &&
+//                 board[i][j] == board[i][j + 3]
+//             ) {
+//                 console.log('column')
+//                 console.log(board[i][j])
+//             }
+//         }
+//     }
+
+// }
+
+// function checkRow() {
+
+
+//     //column
+//     for (let i = 0; i < board.length; i++) {
+//         for (let j = 0; j < board.length - 3; j++) {
+
+//             if (board[j][i] != 0 &&
+//                 board[j][i] == board[j + 1][i] &&
+//                 board[j][i] == board[j + 2][i] &&
+//                 board[j][i] == board[j + 3][i]
+//             ) {
+//                 console.log('row')
+//                 console.log(board[j][i])
+//             }
+
+//         }
+
+//     }
+// }
+
+
+
+function checkColumn() {
 
     //row
-    for (let i = 0; i < board.length; i++) {
-        for (let j = 0; j < board[i].length; j++) {
-            
-            if(board[i][j] != 0 &&
-                board[i][j] == board[i][j + 1] && 
-                board[i][j] == board[i][j + 2] &&
-                board[i][j] == board[i][j + 3]
-                ){
-                    alert('lsdkfj')
-                    console.log(board[i][j])
-                }
+    for (let i = 0; i < columnBtn.length; i++) {
+        for (let j = 0; j < test2[i].length; j++) {
+
+            if (test2[j].classList.contains('testCir') 
+                // test2[j] == test2[j + 1] &&
+                // test2[j] == test2[j + 2] &&
+                // test2[j] == test2[j + 3]
+            ) {
+                console.log('column')
+                console.log(test2[j])
+            }
         }
     }
 
 }
 
-function checkColumn() {
+function checkRow() {
 
 
     //column
     for (let i = 0; i < board.length; i++) {
-        for (let j = 0; j < board.length -3; j++) {
-            
-            if(board[j][i] != 0 &&
-                board[j][i] == board[j + 1][i] && 
+        for (let j = 0; j < board.length - 3; j++) {
+
+            if (board[j][i] != 0 &&
+                board[j][i] == board[j + 1][i] &&
                 board[j][i] == board[j + 2][i] &&
                 board[j][i] == board[j + 3][i]
-                ){
-                    alert('lksdjfs')
-                    console.log(board[j][i])
-                }
-            
+            ) {
+                console.log('row')
+                console.log(board[j][i])
+            }
+
         }
-        
+
     }
 }
-
 function clickCir() {
-    
-    for(let i=0;i<topBtn.length;i++){
-        topBtn[i].addEventListener('click', (e) =>{
-           // console.log(columnBtn[i].dataset.col)
-            //let rightCol = columnBtn[i].dataset.col
-            for(let y=columnBtn[i].length - 1; y>=0; y--){
-                board[i][columnBtn[i].dataset.col] = 1
-                test2[i].classList.add('testCir2')
-                console.log(board)
-           }
 
-        })
+    for (let i = 0; i < columnBtn.length; i++) {
+        for (let j = 0; j < test2.length; j++) {
+            test2[j].addEventListener('click', (e) => {
+    
+           
+                e.target.classList.add('testCir2')
+
+                checkRow()
+                checkColumn()
+               
+            })
+        }
     }
 
-    // for(let i=test2.length -1;i>=0; i--){
-    //     test2[i].addEventListener('click', (e)=>{
-    //         //test2[i].classList.add('testCir2')
-    //         //test2[i].setAttribute('data-red', 1)
-    //         e.target.classList.add('testCir2')
-    //         e.target.setAttribute('data-red', 1)
-    //         //board[i].push(test2[i].dataset.red)
-    //         board[i][parseInt(e.target.dataset.red)] = e.target.dataset.red
-    //         console.log(board)
-    //         console.log(test2[i])
-    //     })
-    // }
 }
 
 
+
+//checkRow()
+//checkColumn()
 clickCir()
+
 //columnFunction()
 
