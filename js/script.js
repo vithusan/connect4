8db25deb8f1516a3a00dcf13665cde80255f4056
Vithusan Kanageswaran <vk@Vithusans-MacBook-Air.local>
2019-10-08 10:33:30 -0400

    added two players turn variable

--- js/script.js
+++ js/script.js
@@ -25,8 +25,9 @@ let board = [
 //     [0, 0, 0, 0, 0, 0, 0]
 // ];
 
-let boardNum
+let player1Turn = true
 const createBoard = () => {
+    
     //clear the createboard function inside the html
     mainBoard.innerHTML = ''
 
@@ -34,31 +35,72 @@ const createBoard = () => {
     for (let row = 0; row < board.length; row++) {
         let rowElement = document.createElement('div')
         rowElement.setAttribute('class', 'row')
+        rowElement.setAttribute('data-row', row + 1)
         mainBoard.appendChild(rowElement)
         let rowBtn = document.getElementsByClassName('row')
-        
+
+
         //creaet divs in each rows
         for (let col = 0; col < board[row].length; col++) {
             let colElement = document.createElement('div')
             colElement.setAttribute('class', 'eachCir empty')
-            colElement.setAttribute('data-col', col)
-            colElement.setAttribute('data-row', row)
+            colElement.setAttribute('data-col', col + 1)
+            //colElement.setAttribute('data-row', row + 1)
             rowElement.appendChild(colElement)
-         
-           for(let i=0; i<rowBtn.length; i++){
-               rowBtn[i].addEventListener('click', () =>{
-                colElement.classList.add('testCir2')   
-                console.log(i)
-               })
-           }
+
+            let colBtn = document.getElementsByClassName('eachCir')
+
+            // console.log(colBtn)
+
+            // rowElement.addEventListener('click', function() {
+
+            //     //e.preventDefault()
+            //     //let num = rowElement.dataset.col
+            //     // for(let i=num.length - 1;i>=0; i--){
+            //     //    console.log(num[i])
+
+            //         colElement.classList.add('testCir2')
+            // //    }
+
+            // })
+
+           
+            colElement.addEventListener('click', function(){
+                if(player1Turn){
+                    colElement.classList.add('red')
+                    board[row][col] = 1
+                    setTimeout(() => {
+                        checkRow()
+                        checkColumn()
+                        
+                    }, 1000)
+                    player1Turn = false
+                }else {
+                    colElement.classList.add('black')
+                board[row][col] = 2
+                setTimeout(() => {
+                    checkRow()
+                    checkColumn()
+                    
+                }, 1000)
+                player1Turn = true
+                }
+                
+            })
 
         }
-    
+
     }
 }
 
 createBoard()
-    
+
+// document.body.addEventListener("click", ".eachCir", function (e) {
+//     e.preventDefault();
+//     let number = $(this).data('col');
+//     console.log(number);
+// });
+
 const reset = () => {
     board = [
         [0, 0, 0, 0],
@@ -82,7 +124,8 @@ function checkColumn() {
                 board[i][j] == board[i][j + 3]
             ) {
                 
-                alert('player 1 won')
+                alert('player1 won')
+                
                 reset()
             }
         }
@@ -102,7 +145,8 @@ function checkRow() {
                 board[j][i] == board[j + 2][i] &&
                 board[j][i] == board[j + 3][i]
             ) {
-                alert('Player 2 won')
+               
+                alert('player2 won')
                 reset()
             }
 
