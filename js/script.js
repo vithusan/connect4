bf835c51fab592bf3c4e4eac046a8cb742f537bb
Vithusan Kanageswaran <vk@Vithusans-MacBook-Air.local>
2019-10-09 01:16:25 -0400

    added column selector

--- js/script.js
+++ js/script.js
@@ -3,54 +3,54 @@ let playersScore = document.querySelector('#playersScore')
 let p1Tag = document.getElementById('p1Tag')
 let p2Tag = document.getElementById('p2Tag')
 
-const nameBox = () =>{
-    let inputDiv = document.querySelector('#popup')
-    let inputDiv2 = document.querySelector('#popup2')
-    let playersNm = document.querySelector('#player1')
-    let playersNm2 = document.querySelector('#player2')
-    let inputName = document.querySelector('#inputName')
-    let inputName2 = document.querySelector('#inputName2')
-    let inName = document.querySelector('.inName')
-    let inputBtn = document.querySelector('#nextBtn')
-    let inputBtn2 = document.querySelector('#nextBtn2')
-    
+// const nameBox = () =>{
+//     let inputDiv = document.querySelector('#popup')
+//     let inputDiv2 = document.querySelector('#popup2')
+//     let playersNm = document.querySelector('#player1')
+//     let playersNm2 = document.querySelector('#player2')
+//     let inputName = document.querySelector('#inputName')
+//     let inputName2 = document.querySelector('#inputName2')
+//     let inName = document.querySelector('.inName')
+//     let inputBtn = document.querySelector('#nextBtn')
+//     let inputBtn2 = document.querySelector('#nextBtn2')
 
 
-    inputDiv.style.display = 'block'
-    inputDiv2.style.display = 'none'
 
-    inputBtn.addEventListener('click', () =>{
+//     inputDiv.style.display = 'block'
+//     inputDiv2.style.display = 'none'
 
-        if(inputName.value == ''){
-            playersNm.innerHTML = `please enter Name`
-            return false
-        }
-        p1Tag.innerText = `${inputName.value}`
-        
-        inputDiv.style.display = 'none'
-        inputDiv2.style.display = 'block'
-        nextFuntion()
-        
-    })
+//     inputBtn.addEventListener('click', () =>{
 
-    const nextFuntion = () =>{
-        inputBtn2.addEventListener('click', () =>{
+//         if(inputName.value == ''){
+//             playersNm.innerHTML = `please enter Name`
+//             return false
+//         }
+//         p1Tag.innerText = `${inputName.value}`
 
-            if(inputName2.value == ''){
-                playersNm2.innerHTML = `please enter Name`
-                return false
-            }
-            p2Tag.innerText = `${inputName2.value}`
-            inputDiv2.style.display = 'none'
+//         inputDiv.style.display = 'none'
+//         inputDiv2.style.display = 'block'
+//         nextFuntion()
 
-        })
-    }
+//     })
 
+//     const nextFuntion = () =>{
+//         inputBtn2.addEventListener('click', () =>{
 
-}
+//             if(inputName2.value == ''){
+//                 playersNm2.innerHTML = `please enter Name`
+//                 return false
+//             }
+//             p2Tag.innerText = `${inputName2.value}`
+//             inputDiv2.style.display = 'none'
+
+//         })
+//     }
 
 
-nameBox()
+// }
+
+
+// nameBox()
 let board = [
     [0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0],
@@ -64,8 +64,9 @@ let pl1Score = 0
 let pl2Score = 0
 let player1Turn = true
 let empArray = []
+
 const createBoard = () => {
-    
+
     //clear the createboard function inside the html
     mainBoard.innerHTML = ''
 
@@ -75,8 +76,8 @@ const createBoard = () => {
         rowElement.setAttribute('class', 'row')
         rowElement.setAttribute('data-row', row + 1)
         mainBoard.appendChild(rowElement)
+        
         let rowBtn = document.getElementsByClassName('row')
-
 
         //creaet divs in each rows
         for (let col = 0; col < board[row].length; col++) {
@@ -94,45 +95,16 @@ const createBoard = () => {
                 colElement.classList.add('black')
             }
 
-            // for (let r = 0; r < rowElement.length; r++) {
-            //     rowElement[r].addEventListener('click', () => {
-            //         checkEmptyColumn(r)
-            //     })
-            // }
+        
+                colElement.addEventListener('click', () => {
+                    checkEmptyColumn(col)
+                })
+        
 
-            colElement.addEventListener('click', function () {
-               
-                console.table(board)
-
-                if (player1Turn) {
-                    board[row][col] = 1
-                    setTimeout(() => {
-                        checkRow()
-                        checkColumn()
-                        //checkLtoR()
-                        //checkRtoL()
-                    }, 300)
-                    p1Tag.style.borderBottom = '2px solid #f76c6c'
-                    player1Turn = false
-                    
-                } else {
-                    board[row][col] = 2
-                    setTimeout(() => {
-                        checkRow()
-                        checkColumn()
-                        //checkLtoR()
-                        //checkRtoL()
-                    }, 300)
-                    p2Tag.style.borderBottom = '2px solid #374785'
-                    player1Turn = true
-                }
-                
-                createBoard()
-                
-
-            })
 
         }
+   
+       
 
     }
 
@@ -142,22 +114,44 @@ const createBoard = () => {
 createBoard()
 
 
+
 const checkEmptyColumn = (column) => {
     // get column when you click 
-    for (let i = board.length - 1; i >= 0; i--) {
-        console.log(`board row ${i} -> ${board[column][i]}`)
+    for (let i = board.length - 1; i>=0; i--) {
+        console.log(`board row ${i} -> ${board[i][column]}`)
         //if empty set to player color chip
         // return after you do that
 
-        for (let c = 0; c < colElement.length; c++) {
-            if (board[column][i] === 0) {
-                colElement[c].classList.add('red')
-                return
-            }
 
+        if (board[i][column] === 0) {
+                 if (player1Turn) {
+                    board[i][column] = 1
+                    setTimeout(() => {
+                        checkRow()
+                        checkColumn()
+                        checkLtoR()
+                        // checkRtoL()
+                    }, 300)
+                    p1Tag.style.borderBottom = '2px solid #f76c6c'
+                    player1Turn = false
 
+                } else {
+                    board[i][column] = 2
+                    setTimeout(() => {
+                        checkRow()
+                        checkColumn()
+                        checkLtoR()
+                        // checkRtoL()
+                    }, 300)
+                    p2Tag.style.borderBottom = '2px solid #374785'
+                    player1Turn = true
+                }
+            return  createBoard()
         }
 
+        // createBoard()
+
+        
 
     }
 }
@@ -191,11 +185,11 @@ function checkColumn() {
                 board[i][j] == board[i][j + 3]
             ) {
                 if (player1Turn) {
-                    
+
                     pl2Score += 1
-                    
+
                 } else {
-                    
+
                     pl1Score += 1
                 }
 
@@ -207,41 +201,43 @@ function checkColumn() {
 
 }
 
-// function checkLtoR() {
+function checkLtoR() {
 
-//     //column
-//     for (let i = 0; i < board.length; i++) {
-//         for (let j = 0; j < board[i].length; j++) {
+    //column
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board[i].length; j++) {
 
-//             if (board[i][j] != 0 &&
-//                 board[i][j] == board[i -1][j + 1] &&
-//                 board[i][j] == board[i-2][j + 2] &&
-//                 board[i][j] == board[i-3][j + 3]
-//             ) {
-//                 if (player1Turn) {
-//                     alert('player 2 won')
-//                 } else {
-//                     alert('player 1 won')
-//                 }
+            if (board[i][j] != 0 &&
+                board[i][j] == board[i-1][j + 1] &&
+                board[i][j] == board[i-2][j + 2] &&
+                board[i][j] == board[i-3][j + 3]
+            ) {
+                if (player1Turn) {
 
+                    pl2Score += 1
 
-//                 reset()
-//             }
-//         }
-//     }
+                } else {
 
-// }
+                    pl1Score += 1
+                }
+
+                reset()
+            }
+        }
+    }
+
+}
 
 // function checkRtoL() {
 
 //     //column
-//     for (let i = 0; i < board.length; i++) {
-//         for (let j = 0; j < board.length -3; j++) {
+//     for (let i = 0; i < board.length -3; i++) {
+//         for (let j = 3; j < board[i].length; j++) {
 
-//             if (board[j][i] != 0 &&
-//                 board[j][i] == board[j-1][i + 1] &&
-//                 board[j][i] == board[j-2][i + 2] &&
-//                 board[j][i] == board[j-3][i + 3]
+//             if (board[i][j] != 0 &&
+//                 board[i][j] == board[i+1][j - 1] &&
+//                 board[i][j] == board[i+2][j - 2] &&
+//                 board[i][j] == board[i+3][j - 3]
 //             ) {
 //                 if (player1Turn) {
 //                     alert('player 2 won')
@@ -270,10 +266,10 @@ function checkRow() {
                 board[j][i] == board[j + 3][i]
             ) {
                 if (player1Turn) {
-                    
+
                     pl2Score += 1
                 } else {
-                    
+
                     pl1Score += 1
                 }
                 reset()
